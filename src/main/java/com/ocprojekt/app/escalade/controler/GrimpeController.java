413d8a15e294a7cbf909b0db9fd8a1cdb0ac3786
Moorgtat <adgjadgj2502>
2018-10-11 08:58:09 +0200

    Ajout home controller.

--- src/main/java/com/ocprojekt/app/escalade/controler/GrimpeController.java
+++ src/main/java/com/ocprojekt/app/escalade/controler/GrimpeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
-public class EscController {
+public class GrimpeController {
 
     @Autowired
     private SiteRepository siteRepository;
@@ -25,15 +25,16 @@ public class EscController {
     @Autowired
     private VoieRepository voieRepository;
 
-    @RequestMapping(value="/Escaladons")
-    public String Escaladons(Model model){
+
+    @RequestMapping(value="/Grimpe")
+    public String Grimpe(Model model){
         List<Site> lsite = siteRepository.findAll();
         model.addAttribute("site" , lsite );
         List<Secteur> lsecteur = secteurRepository.findAll();
         model.addAttribute("secteur", lsecteur);
         List<Voie> lvoie = voieRepository.findAll();
         model.addAttribute("voie", lvoie);
-        return "Escaladons";
+        return "Grimpe";
     }
 
 
