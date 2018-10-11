4b865570356834821ada3912ca4b0bdecfe31a47
Moorgtat <adgjadgj2502>
2018-10-11 09:12:45 +0200

    Ajout home controller.

--- src/main/java/com/ocprojekt/app/escalade/controler/HomeController.java
+++ src/main/java/com/ocprojekt/app/escalade/controler/HomeController.java
@@ -1,5 +1,9 @@
 package com.ocprojekt.app.escalade.controler;
 
+import com.ocprojekt.app.escalade.repository.SecteurRepository;
+import com.ocprojekt.app.escalade.repository.SiteRepository;
+import com.ocprojekt.app.escalade.repository.VoieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +11,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
+    @Autowired
+    private SiteRepository siteRepository;
+
+    @Autowired
+    private SecteurRepository secteurRepository;
+
+    @Autowired
+    private VoieRepository voieRepository;
+
+
     @RequestMapping(value = "/Home")
-    public String Home(Model model) {
+    public String Home(Model mod) {
         return "Home";
     }
+
+
 }
